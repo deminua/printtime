1faad179720ba1889f0849848d580ffbaf4c741d
deminua <dataranenko@gmail.com.ua>
2016-05-29 22:03:53 +0300

    Локальное подключение пакета PrinttimeAdminProvider через vendor

--- config/app.php
+++ config/app.php
@@ -159,6 +159,7 @@ return [
         Barryvdh\Debugbar\ServiceProvider::class,
 
         Intervention\Image\ImageServiceProvider::class,
+        Printtime\PtAdmin\PrinttimeAdminProvider::class,
     ],
 
     /*
